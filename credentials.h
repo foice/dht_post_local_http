1ae5bd46a2c09b05b163745b96c82f973faf0aec
foice <foice.news@gmail.com>
2020-12-08 23:17:39 +0100

    first commit

---
+++ credentials.h
@@ -0,0 +1,9 @@
+//WiFi Connection configuration
+const char *OTAName = "name";         // A name and a password for the OTA service
+const char *OTAPassword = "password";
+// *************************************
+// ################ WiFi Client ################
+const char* ssid     = "essid";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "alongpassword";     
+
+const char* appassword = "accespoint"; // I will broadcast the encryption of the network 
