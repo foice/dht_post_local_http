b214182d5bee4f2c1218be6d0e85cec6472bba8c
foice <foice.news@gmail.com>
2025-09-08 00:11:12 +0200

    Update credentials.h

--- credentials.h
+++ credentials.h
@@ -7,3 +7,10 @@ const char* ssid     = "essid";         // The SSID (name) of the Wi-Fi network 
 const char* password = "alongpassword";     
 
 const char* appassword = "accespoint"; // I will broadcast the encryption of the network 
+
+IPAddress mqttServer(192,168,x,x);
+char *mqtt_server = "192.168.x.x";
+char *mqtt_username = "user";
+char *mqtt_password = "pass";
+
+char *device_name = "DHT"; // can be freely set, e.g. your name
